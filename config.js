63eb758d1fd61eed6019fc7a7538b8b9adebf54b
MoOnsy07 <moonsyzonkol@gmail.com>
2026-08-25 03:43:42 +0300

    Load PayPal support addon

--- config.js
+++ config.js
@@ -15,3 +15,13 @@ const DONATION_INSTAPAY = 'medoonsy@instapay';
 const DONATION_INSTAPAY_LINK = 'https://ipn.eg/S/medoonsy/instapay/2DrTZL';
 const DONATION_PAYPAL_EMAIL = 'Medozonkol@gmail.com';
 const DONATION_TITLE = 'ادعم طرف الخيط ❤️';
+
+// إضافة PayPal منفصلة عشان تفضل نافذة الدعم الأساسية بسيطة وسهلة التعديل.
+(function(){
+  try{
+    const s=document.createElement('script');
+    s.src='donation-paypal-addon.js?v=20260825-1';
+    s.defer=true;
+    document.head.appendChild(s);
+  }catch(e){}
+})();
