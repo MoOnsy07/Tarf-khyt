2e166b5b7bb59a358537109d7488ed8434d9ed71
MoOnsy07 <moonsyzonkol@gmail.com>
2026-09-03 17:48:42 +0300

    add Last Seen assets 41-50 and asset registry

---
+++ v2/last-seen-assets.js
@@ -0,0 +1,18 @@
+(() => {
+  'use strict';
+
+  const BASE = '../images/last-seen/';
+
+  window.LAST_SEEN_ASSETS = Object.freeze({
+    evidenceBackupDrive: BASE + '41_evidence_backup_drive.webp',
+    endingBuriedTruth: BASE + '42_ending_buried_truth.webp',
+    endingKeepOnline: BASE + '43_ending_keep_online.webp',
+    endingCutThread: BASE + '44_ending_cut_thread.webp',
+    endingPerfectSubject: BASE + '45_ending_perfect_subject.webp',
+    endingUnpredictable: BASE + '46_ending_unpredictable.webp',
+    apartmentHallway: BASE + '47_apartment_hallway.webp',
+    policeCorridor: BASE + '48_police_corridor.webp',
+    nightDriveIndustrial: BASE + '49_night_drive_industrial.webp',
+    blankSurveillanceNoise: BASE + '50_blank_surveillance_noise.webp'
+  });
+})();
