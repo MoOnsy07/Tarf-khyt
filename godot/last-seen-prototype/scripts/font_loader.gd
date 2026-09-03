b5105f1c9ad9113d758ccb5fbe622fa129940090
MoOnsy07 <moonsyzonkol@gmail.com>
2026-09-03 23:01:37 +0300

    load Arabic font as a baked Godot resource

--- godot/last-seen-prototype/scripts/font_loader.gd
+++ godot/last-seen-prototype/scripts/font_loader.gd
@@ -2,22 +2,10 @@ class_name ArabicFontLoader
 extends RefCounted
 
 static func get_font() -> FontFile:
-    var encoded := ""
-    for index in range(8):
-        var path := "res://fonts/embedded/part_%02d.b64" % index
-        var file := FileAccess.open(path, FileAccess.READ)
-        if file == null:
-            push_error("Missing embedded Arabic font chunk: %s" % path)
-            return null
-        encoded += file.get_as_text().strip_edges()
-        file.close()
-
-    var bytes := Marshalls.base64_to_raw(encoded)
-    if bytes.size() < 1000:
-        push_error("Embedded Arabic font decode failed")
-        return null
-
-    var font := FontFile.new()
-    font.data = bytes
-    font.allow_system_fallback = false
-    return font
+    var resource := load("res://fonts/NotoSansArabic.ttf")
+    if resource is FontFile:
+        var font := resource as FontFile
+        font.allow_system_fallback = false
+        return font
+    push_error("Embedded Arabic font resource is missing")
+    return null
